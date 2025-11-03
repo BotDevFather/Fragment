3b1f837e6e183519f02c0e0b1ed8ac4e2330827f
Suraj Kumar <148668739+BotDevFather@users.noreply.github.com>
2025-11-03 17:19:50 +0530

    Update fragment.js

--- api/fragment.js
+++ api/fragment.js
@@ -70,9 +70,9 @@ export default async function handler(req, res) {
     drawText(data.ton_web3_address, 50, 90, 32, "#ffffff", "600");
     drawText(data.status, 300, 80, 23, "#5FE890", "600");
     drawText(data.current_high_bid, 325, 280, 24, "#ffffff", "700");
-    drawText(data.username, 1130, 80, 28, "#22A9D8", "600");
-    drawText(data.web_address, 1070, 180, 28, "#22A9D8", "700");
-    drawText(data.ton_web3_address, 1070, 270, 28, "#22A9D8", "600");
+    drawText(data.username, 1100, 80, 28, "#22A9D8", "600");
+    drawText(data.web_address, 1060, 180, 28, "#22A9D8", "700");
+    drawText(data.ton_web3_address, 1060, 270, 28, "#22A9D8", "600");
 
     // 🧩 Draw bid history
     let y = 370;
